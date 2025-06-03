c561551f69b7eef298a736b52866b1652b84efc1
tothgergo200 <tothgergo200@gmail.com>
2025-06-03 15:46:57 +0200

    provider, variables set, .gitignore created

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the infrastructure"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+  default     = "Iso_G"
+}
