f547c90e6d31901c9ff5364bb22c3c7ba70f0654
tothgergo200 <tothgergo200@gmail.com>
2025-06-03 16:46:16 +0200

    added route table association in main.tf  and created security.tf

--- main.tf
+++ main.tf
@@ -38,3 +38,9 @@ resource "aws_route_table" "public" {
     }
 }
 
+resource "aws_route_table_association" "public_assoc" {
+    subnet_id      = aws_subnet.public_subnet.id
+    route_table_id = aws_route_table.public.id
+}
+
+
