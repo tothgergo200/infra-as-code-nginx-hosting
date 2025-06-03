09ea6f2ff35ee63a90cf379ef0623c10dd4be65d
tothgergo200 <tothgergo200@gmail.com>
2025-06-03 16:18:02 +0200

    main.tf done

---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "aws_vpc" "main" {
+    cidr_block       = "10.0.0.0/16"
+    instance_tenancy = "default"
+
+    tags = {
+        Name = "main-vpc"
+    }
+}
+
+resource "aws_subnet" "public_subnet" {
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone       = "${var.aws_region}a"
+
+    tags = {
+        Name = "public-subnet"
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "main-igw"
+    }
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route{
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+    tags{
+        Name = "main-route-table"
+    }
+}
+
